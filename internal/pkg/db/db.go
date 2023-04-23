1e3e5a8fe3e0a899bf95d2f21b3d963ce22edc12
Hao.Wu <1593774151@qq.com>
2023-04-23 15:43:06 +0800

    Add db client config

---
+++ internal/pkg/db/db.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	conf "go-template/internal/conf"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"time"
+)
+
+type Config struct {
+	DSN             string // write data source name.
+	MaxOpenConn     int    // open pool
+	MaxIdleConn     int    // idle pool
+	ConnMaxLifeTime int
+}
+
+var DB *gorm.DB
+
+// InitDbConfig 初始化Db
+func InitDbConfig(c *conf.Data) {
+	log.Info("Initializing Mysql")
+	var err error
+	dsn := c.Database.Dsn
+	maxIdleConns := c.Database.MaxIdleConn
+	maxOpenConns := c.Database.MaxOpenConn
+	connMaxLifetime := c.Database.ConnMaxLifeTime
+	if DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		QueryFields: true,
+		NamingStrategy: schema.NamingStrategy{
+			//TablePrefix:   "",   // 表名前缀
+			SingularTable: true, // 使用单数表名
+		},
+	}); err != nil {
+		panic(fmt.Errorf("初始化数据库失败: %s \n", err))
+	}
+	sqlDB, err := DB.DB()
+	if sqlDB != nil {
+		sqlDB.SetMaxIdleConns(int(maxIdleConns))                               // 空闲连接数
+		sqlDB.SetMaxOpenConns(int(maxOpenConns))                               // 最大连接数
+		sqlDB.SetConnMaxLifetime(time.Second * time.Duration(connMaxLifetime)) // 单位：秒
+	}
+	log.Info("Mysql: initialization completed")
+}
