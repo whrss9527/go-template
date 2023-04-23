57b5ee8c71ab34df99afab8c259b5ea1b0227e3b
Hao.Wu <1593774151@qq.com>
2023-04-23 15:40:02 +0800

    Add biz test

---
+++ internal/biz/template_test.go
@@ -0,0 +1,15 @@
+package biz
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestUserUsecase_RefreshVitality(t *testing.T) {
+	Convey("test refresh vitality", t, func() {
+		Convey("ok", func() {
+			// prepare
+
+		})
+	})
+}
