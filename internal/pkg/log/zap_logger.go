a666f3e5b6aff1720849043e56c34924ebc8c372
Hao.Wu <1593774151@qq.com>
2023-09-26 15:25:58 +0800

    feat: customize logger

---
+++ internal/pkg/log/zap_logger.go
@@ -0,0 +1,108 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var _ log.Logger = (*ZapLogger)(nil)
+
+type ZapLogger struct {
+	log  *zap.Logger
+	Sync func() error
+}
+
+// Logger 配置zap日志,将zap日志库引入
+func Logger() log.Logger {
+	//配置zap日志库的编码器
+	encoder := zapcore.EncoderConfig{
+		TimeKey:       "time",
+		LevelKey:      "level",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		MessageKey:    "msg",
+		StacktraceKey: "stack",
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format("2006-01-02 15:04:05"))
+		},
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder,
+	}
+	return NewZapLogger(
+		"",
+		encoder,
+		zap.NewAtomicLevelAt(zapcore.DebugLevel),
+		zap.AddStacktrace(
+			zap.NewAtomicLevelAt(zapcore.ErrorLevel)),
+		zap.AddCaller(),
+		zap.AddCallerSkip(2),
+		zap.Development(),
+	)
+}
+
+// 日志自动切割，采用 lumberjack 实现的
+func getLogWriter(fileName string) zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   fileName, //指定日志存储位置
+		MaxSize:    10,       //日志的最大大小（M）
+		MaxBackups: 5,        //日志的最大保存数量
+		MaxAge:     30,       //日志文件存储最大天数
+		Compress:   false,    //是否执行压缩
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
+
+// NewZapLogger return a zap logger.
+func NewZapLogger(fileName string, encoder zapcore.EncoderConfig, level zap.AtomicLevel, opts ...zap.Option) *ZapLogger {
+	//日志切割
+	writeSyncer := getLogWriter(fileName)
+	//设置日志级别
+	level.SetLevel(zap.InfoLevel)
+	var core zapcore.Core
+	//开发模式下打印到标准输出
+	// --根据配置文件判断输出到控制台还是日志文件--
+	core = zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoder), // 编码器配置
+		//zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)), // 打印到控制台
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(writeSyncer)), // 打印到控制台和文件
+		level, // 日志级别
+	)
+
+	zapLogger := zap.New(core, opts...)
+	return &ZapLogger{log: zapLogger, Sync: zapLogger.Sync}
+}
+
+// Log 实现log接口
+func (l *ZapLogger) Log(level log.Level, keyvals ...interface{}) error {
+	if len(keyvals) == 0 || len(keyvals)%2 != 0 {
+		l.log.Warn(fmt.Sprint("Keyvalues must appear in pairs: ", keyvals))
+		return nil
+	}
+
+	var data []zap.Field
+	for i := 0; i < len(keyvals); i += 2 {
+		data = append(data, zap.Any(fmt.Sprint(keyvals[i]), keyvals[i+1]))
+	}
+
+	switch level {
+	case log.LevelDebug:
+		l.log.Debug("", data...)
+	case log.LevelInfo:
+		l.log.Info("", data...)
+	case log.LevelWarn:
+		l.log.Warn("", data...)
+	case log.LevelError:
+		l.log.Error("", data...)
+	case log.LevelFatal:
+		l.log.Fatal("", data...)
+	}
+	return nil
+}
